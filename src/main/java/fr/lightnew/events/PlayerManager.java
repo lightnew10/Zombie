577661fe10f5c541be6d0569a74ba321e2ad9ff1
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 15:02:43 +0200

    item

--- src/main/java/fr/lightnew/events/PlayerManager.java
+++ src/main/java/fr/lightnew/events/PlayerManager.java
@@ -1,6 +1,7 @@
 package fr.lightnew.events;
 
 import fr.lightnew.commands.Build;
+import fr.lightnew.tools.ItemBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,8 +11,11 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerManager implements Listener {
+
+    public static ItemStack skull_armorstand = ItemBuilder.skull(1, "lightnew", "lightnew");
 
     @EventHandler
     public void join(PlayerJoinEvent event) {
