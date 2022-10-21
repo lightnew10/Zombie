c5d2ade9fb3521b77f04c16779f0b5fa53f6f3b0
BuildTools <unconfigured@null.spigotmc.org>
2022-10-21 20:41:40 +0200

    spawning armorStand and meta

--- src/main/java/fr/lightnew/tools/HeadClick.java
+++ src/main/java/fr/lightnew/tools/HeadClick.java
@@ -1,15 +1,15 @@
 package fr.lightnew.tools;
 
-import fr.lightnew.ZombieMain;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.server.v1_16_R3.*;
-import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HeadClick {
 
@@ -17,35 +17,23 @@ public class HeadClick {
         //NE SPAWN PAS
         WorldServer s = ((CraftWorld)player.getWorld()).getHandle();
         EntityArmorStand armorStand = new EntityArmorStand(EntityTypes.ARMOR_STAND, s);
-        Location location = new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());
-        double baseY = location.getY();
 
-        armorStand.setCustomNameVisible(true);
-        armorStand.setNoGravity(true);
         armorStand.setCustomName(new ChatMessage(name));
-        armorStand.setBasePlate(false);
-        armorStand.setSmall(true);
+        armorStand.setCustomNameVisible(true);
         armorStand.setInvulnerable(true);
-        armorStand.setSlot(EnumItemSlot.HEAD, CraftItemStack.asNMSCopy(skull));
+        armorStand.setInvisible(true);
+        armorStand.setSmall(true);
+        armorStand.setLocation(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());
 
-        //PacketPlayOutEntityEquipment EquipP = new PacketPlayOutEntityEquipment(armorStand.getId(), EnumItemSlot.HEAD.getSlotFlag(), CraftItemStack.asNMSCopy(skull));
+        List<Pair<EnumItemSlot, net.minecraft.server.v1_16_R3.ItemStack>> list = new ArrayList<>();
+        list.add(new Pair<>(EnumItemSlot.HEAD, CraftItemStack.asNMSCopy(skull)));
+
+        PacketPlayOutEntityEquipment EquipP = new PacketPlayOutEntityEquipment(armorStand.getId(), list);
+        PacketPlayOutEntityMetadata standMeta = new PacketPlayOutEntityMetadata(armorStand.getId(), armorStand.getDataWatcher(), true);
         PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand);
         ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
-        BukkitTask task = new BukkitRunnable() {
-            double add = 0.1;
-            @Override
-            public void run() {
-
-                armorStand.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
-
-                if (add >= (baseY+1))
-                    add--;
-                else if (add <= (baseY-1))
-                    add++;
-                else
-                    add--;
-            }
-        }.runTaskTimer(ZombieMain.instance, 10, 0);
+        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(EquipP);
+        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(standMeta);
         return armorStand;
     }
 }
